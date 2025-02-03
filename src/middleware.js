d2fd34ef5a67bc5c10f331854dc3aa257db5081f
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2025-02-03 15:09:48 +0100

    ✨ Update login token expiration and adjust middleware URL path for improved authentication flow

--- src/middleware.js
+++ src/middleware.js
@@ -3,14 +3,14 @@ import { cookies } from 'next/headers';
 
 export async function middleware(request) {
   const url = new URL(request.url);
-  url.pathname = "/auth/login";
+  url.pathname = "/login";
   const cookiesStore = await cookies();
   const token = cookiesStore.get('token') || undefined;
-  console.log(token, 'TOKEN FROM MIDDLEWARE');
   if (!token) {
     // verify the token valid
     return NextResponse.redirect(url.toString());
   }
+  return NextResponse.next();
 }
 
 export const config = {
